d629c2d7108bdaf063c40ab275d833f776bc2031
Vitaly Shvedov <mr_gold1k@mail.ru>
2023-04-08 13:43:46 +0300

    ps

--- jspage2.js
+++ jspage2.js
@@ -1,4 +1,16 @@
-var basket = 1;
+//Объявление переменных//
+var basket = 0 + "р.";
+var salat_price1 = 400 + "р.";
+var salat_price2 = 250 + "р.";
+var pizza_price1 = 550 + "р.";
+var pizza_price2 = 450 + "р.";
+var pizza_price3 =  600 + "р.";
+var pasta_price = 300 + "р.";
+var water_price1 = 100 + "р.";
+var water_price2 = 150 + "р.";
+var water_price3 = 200 + "р.";
+
+
 
 //Онклик на всю страницу
 document.addEventListener("click", function(event) {
