c6f4571d5bbbb6522e37734811cbbfbbdc4678e8
Eremenko Nikita <neremenko191@gmail.com>
2023-04-09 00:59:24 +0300

    dfbdh

---
+++ Site_final/knopa.js
@@ -0,0 +1,12 @@
+//отслеживаем клик на на странице
+window.addEventListener('clik', function (event){
+    if (event.target.hasAttribute('btn')){
+        console.log('Click on cart button')
+    
+    }
+})
+
+
+
+
+
