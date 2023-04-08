5bfeaf689ec37a000f0086153951c813090e3e49
Vitaly Shvedov <mr_gold1k@mail.ru>
2023-04-08 09:49:07 +0300

    apdate

---
+++ jspage2.js
@@ -0,0 +1,18 @@
+var basket = 1;
+
+//Онклик на всю страницу
+document.addEventListener("click", function(event) {
+    // Нужный код
+    console.log("Клик на странице");
+  });
+
+
+  //Изменяем переменную
+  let selectedValue = 0; // начальное значение переменной
+const selectedValueElement = document.getElementById("selectedValue"); // получаем элемент, в котором будем отображать значение переменной
+
+function changeValue(value) { // функция для изменения значения переменной
+  selectedValue = value; // присваиваем переменной новое значение
+  selectedValueElement.textContent = selectedValue; // обновляем значение на странице
+}
+
